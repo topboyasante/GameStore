f7376a83f7fb11cb9b5743945137b5ca82a45b8b
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-05-31 23:39:14 +0100

    Progress: Created Endpoints to look for a specific game and add new games.

--- gamestore.api/Program.cs
+++ gamestore.api/Program.cs
@@ -1,7 +1,9 @@
 using gamestore.api.entities;
 
+const string GetGameEndPointName = "GetGame";
+
 //An instance of the game entity
-List<Game> games = new();
+List<Game> games = new() 
 {
     new Game()
     {
@@ -11,7 +13,16 @@ List<Game> games = new();
         Price = 19.99M,
         releaseDate= new DateTime(1991,2,1),
         ImageURI = "https://placehold.co/100"
-    };
+    },
+    new Game()
+    {
+        ID=2,
+        Name="FIFA 23",
+        Genre="Sports",
+        Price = 19.99M,
+        releaseDate= new DateTime(2022,9,1),
+        ImageURI = "https://placehold.co/100"
+    }
 };
 
 //Create the builder object
@@ -21,6 +32,27 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //Define your endpoint with the app pipeline
-app.MapGet("/", () => "Hello Worldd!");
+
+//Get Request for all Games
+app.MapGet("/games", () => games);
+
+//Get Request for one Game
+app.MapGet("/games/{id}",(int id)=> {
+    Game? gameInQuestion = games.Find(game => game.ID == id);
+
+    if(gameInQuestion is null){
+        return Results.NotFound();
+    }
+    else{
+        return Results.Ok(gameInQuestion);
+    }
+}).WithName(GetGameEndPointName);
+
+app.MapPost("/games", (Game game)=>{
+    game.ID = games.Max(game => game.ID) + 1;
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndPointName, new {ID = game.ID}, game);
+});
 
 app.Run();
